4b18114dd2f0695bdba6de7cb1221380b0b89308
Benoit Pierre <benoit.pierre@gmail.com>
2021-05-26 19:34:34 +0200

    lint: unnecessary-pass

--- plover_stenograph_usb.py
+++ plover_stenograph_usb.py
@@ -461,27 +461,22 @@ else:
 
 class ProtocolViolationException(Exception):
     """The writer did something unexpected"""
-    pass
 
 
 class UnableToPerformRequestException(Exception):
     """The writer cannot perform the action requested"""
-    pass
 
 
 class FileNotAvailableException(Exception):
     """The writer cannot read from the current file"""
-    pass
 
 
 class NoRealtimeFileException(Exception):
     """The realtime file doesn't exist, likely because the user hasn't started writing"""
-    pass
 
 
 class FinishedReadingClosedFileException(Exception):
     """The closed file being read is complete and cannot be read further"""
-    pass
 
 
 class Stenograph(ThreadedStenotypeBase):
